0f299fc51991b760b29ebf0891e69a0e2bf3f236
Philipp <e13630@dees.eberspaecher.com>
2019-12-02 16:34:34 +0100

    connected client and server using socket io

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import io from "socket.io-client";
 import './App.css';
 
 function App() {
+
+  const socket = io(":5000");
+  
+  socket.on("message", () => {
+    console.log("message");
+  })
+
   return (
     <div className="App">
       <header className="App-header">
--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const ioSocket = require("socket.io")(server);
 
-ioSocket.on("connection", socket => {
-    console.log(socket.id);
+ioSocket.on("connection", client => {
+   console.log(client.id);
+});
+
+app.get("/", (req, res) => {
+    console.log("yay")
+    res.send("HALLO!")
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log("Listening");
 });
