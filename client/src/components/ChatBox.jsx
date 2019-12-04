aa4adb5be2b7e9dc8276e4cfae3e9c0fd7a2f4fc
Tisciit <tisciit@gmail.com>
2019-12-04 10:09:04 +0100

    removed socket, added useEffect to utilize api functions

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,20 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { subscribeChat, unsubscribeChat, sendChat, changeName } from "../api";
+import "./ChatBox.css";
 
-export const ChatBox = (props) => {
+export const ChatBox = () => {
 
-    const { socket } = props;
+    //Reveice Chat events
 
     const [message, setMessage] = useState("This is a test");
+    const [history, setHistory] = useState([]);
+
+
+    useEffect(() => {
+        subscribeChat((data) => {
+            console.log("received");
+            const tmp = [...history, <p key={data.id}>{data.user}: {data.message}</p>];
+            setHistory(tmp);
+            //console.log(tmp);
+            unsubscribeChat();
+            //Will be re-applied when effect is run again
+        });
+    }, [history]);
+
 
     const sendMsg = () => {
-        console.log(socket);
-        socket.emit("message", message);
+        if (message.startsWith("/")) {
+            //This is a command
+            const command = message.substr(1);
+            const split = command.split(" ");
+            switch (split[0]) {
+                case "name":
+                    changeName(split[1]);
+                    break;
+            }
+        }
+        else {
+            sendChat(message);
+        }
+
+        setMessage("");
     }
 
     return (
         <div className="ChatWrapper">
-            <div className="Chathistory" />
-            <input type="text" name="" id="" />
+            <div className="Chathistory">
+                {history.map(elt => elt)}
+            </div>
+            <input onKeyDown={(e) => { if (e.keyCode === 13) { sendMsg() } }} onChange={(e) => { setMessage(e.target.value) }} value={message} />
             <button onClick={sendMsg}>Send</button>
         </div>
     )
