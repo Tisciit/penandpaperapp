8cdb79fa9ab4480a1cf557f14c6aec6d651b105b
Lohr <e13630@dees.eberspaecher.com>
2019-12-05 09:19:10 +0100

    changed socket events to use variables instead of text

--- server.js
+++ server.js
@@ -2,36 +2,41 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const ioSocket = require("socket.io")(server);
 
-
-
 const chatHistory = [];
 
 ioSocket.on("connection", client => {
+  //#region Command Variables
+  /**
+   * Write all in UPPERCASE
+   * Prefix "Commands" with C_
+   *  
+   */
+  const CHAT = "CHAT";
+  const C_NAME = "/NAME";
+  //#endregion
 
-    //#region Events for "/" commands
-    client.on("/name", newName => {
-        client.name = newName;
-    });
+  //#region Events for "/" commands
+  client.on(C_NAME, newName => {
+    client.name = newName;
+  });
 
-    client.on("CHAT", (message) => {
-        let id = chatHistory.push({ "client": client.id, message });
-        const data = {
-            id,
-            user: client.name || client.id,
-            message: message
-        }
+  client.on(CHAT, message => {
+    let id = chatHistory.push({ client: client.id, message });
+    const data = {
+      id,
+      user: client.name || client.id,
+      message: message
+    };
 
-        ioSocket.emit("CHAT", data);
-    });
+    ioSocket.emit(CHAT, data);
+  });
 });
 
-
-
 app.get("/", (req, res) => {
-    res.send(JSON.stringify(chatHistory));
+  res.send(JSON.stringify(chatHistory));
 });
 
 const PORT = 5000;
 server.listen(PORT, () => {
-    console.log("Listening");
+  console.log("Listening");
 });
