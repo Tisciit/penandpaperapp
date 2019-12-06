0047946e18571ddb6eb7f761e279edc09851b5c8
Lohr <e13630@dees.eberspaecher.com>
2019-12-06 14:02:56 +0100

    added auto scroll on new chat message

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,58 +1,79 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { subscribeChat, unsubscribeChat, sendChat, changeName } from "../api";
 import "./ChatBox.css";
 
+const scrollToBottom = obj => obj.scrollTo(0, obj.scrollHeight);
+const isScrolledBottom = obj => {
+  return obj.scrollHeight - obj.scrollTop === obj.clientHeight;
+};
+
 export const ChatBox = () => {
+  //Reveice Chat events
 
-    //Reveice Chat events
+  const refHistory = useRef(null);
+  const [message, setMessage] = useState("This is a test");
+  const [history, setHistory] = useState([]);
 
-    const [message, setMessage] = useState("This is a test");
-    const [history, setHistory] = useState([]);
+  useEffect(() => {
+    subscribeChat(data => {
+      console.log("received");
+      const tmp = [
+        ...history,
+        <p key={data.id}>
+          {data.user}: {data.message}
+        </p>
+      ];
+      setHistory(tmp);
+      //console.log(tmp);
+      unsubscribeChat();
+      //Will be re-applied when effect is run again
+    });
+  }, [history]);
 
+  useEffect(() => {
+    scrollToBottom(refHistory.current);
+  }, [history]);
 
-    useEffect(() => {
-        subscribeChat((data) => {
-            console.log("received");
-            const tmp = [...history, <p key={data.id}>{data.user}: {data.message}</p>];
-            setHistory(tmp);
-            //console.log(tmp);
-            unsubscribeChat();
-            //Will be re-applied when effect is run again
-        });
-    }, [history]);
-
-
-    const sendMsg = () => {
-        if (message == "") {
-            return;
-        }
-
-        if (message.startsWith("/")) {
-            //This is a command
-            const command = message.substr(1);
-            const split = command.split(" ");
-            switch (split[0]) {
-                case "name":
-                    changeName(split[1]);
-                    break;
-            }
-        }
-        else {
-            sendChat(message);
-        }
-
-        setMessage("");
+  const sendMsg = () => {
+    if (message == "") {
+      return;
     }
 
-    return (
-        <div className="ChatWrapper">
-            <div className="Chathistory">
-                {history.map(elt => elt)}
-            </div>
-            <div>
-                <input onKeyDown={(e) => { if (e.keyCode === 13) { sendMsg() } }} onChange={(e) => { setMessage(e.target.value) }} value={message} />
-                <button onClick={sendMsg}>Send</button>
-            </div>
-        </div>
-    )
-}
+    if (message.startsWith("/")) {
+      //This is a command
+      const command = message.substr(1);
+      const split = command.split(" ");
+      switch (split[0]) {
+        case "name":
+          changeName(split[1]);
+          break;
+      }
+    } else {
+      sendChat(message);
+    }
+
+    setMessage("");
+  };
+
+  return (
+    <div className="ChatWrapper">
+      <div ref={refHistory} className="Chathistory">
+        {history.map(elt => elt)}
+      </div>
+      <div>
+        <input
+          onKeyDown={e => {
+            if (e.keyCode === 13) {
+              sendMsg();
+            }
+          }}
+          onChange={e => {
+            setMessage(e.target.value);
+          }}
+          value={message}
+        />
+        <button onClick={sendMsg}>Send</button>
+      </div>
+    </div>
+  );
+};
