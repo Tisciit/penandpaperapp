deead02ca1a89c2e3d60f96f5ccbd137bdb92d1c
Lohr <e13630@dees.eberspaecher.com>
2019-12-06 11:45:19 +0100

    changed strings to consts, added audio api functions

--- client/src/api.js
+++ client/src/api.js
@@ -1,22 +1,34 @@
 import io from "socket.io-client";
 const socket = io(":5000");
 
-const CHAT = "CHAT"
+const CHAT = "CHAT";
+const AUDIO = "AUDIO";
+const C_NAME = "/NAME";
 
-const subscribeChat = (cb) => {
-    socket.on(CHAT, data => cb(data));
-}
+const subscribeChat = cb => {
+  socket.on(CHAT, data => cb(data));
+};
 
 const unsubscribeChat = () => {
-    socket.off(CHAT);
-}
+  socket.off(CHAT);
+};
 
-const sendChat = (message) => {
-    socket.emit(CHAT, message);
-}
+const sendChat = message => {
+  socket.emit(CHAT, message);
+};
 
-const changeName = (newName) => {
-    socket.emit("/name", newName);
-}
+const changeName = newName => {
+  socket.emit(C_NAME, newName);
+};
 
-export { subscribeChat, unsubscribeChat, sendChat, changeName };
+export const audioURL = ("http://localhost:5000/audio");
+
+const subscribeAudio = cb => {
+  socket.on(AUDIO, data => cb(data));
+};
+
+const unsubscribeAudio = () => {
+  socket.off(AUDIO);
+};
+
+export { subscribeChat, unsubscribeChat, sendChat, changeName, subscribeAudio, unsubscribeAudio };
