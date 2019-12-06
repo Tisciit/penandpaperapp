e774af920d5bbaf5b823bf9c83fa36b2691eb2c8
Lohr <e13630@dees.eberspaecher.com>
2019-12-06 14:25:39 +0100

    AudioPlayer: added own buttons for pause play volume; api: added dice events

--- client/src/api.js
+++ client/src/api.js
@@ -3,32 +3,45 @@ const socket = io(":5000");
 
 const CHAT = "CHAT";
 const AUDIO = "AUDIO";
+const DICE_ROLL = "DICE_ROLL"
+const DICE_RESU = "DICE_RESULT"
 const C_NAME = "/NAME";
 
-const subscribeChat = cb => {
+
+export const subscribeChat = cb => {
   socket.on(CHAT, data => cb(data));
 };
 
-const unsubscribeChat = () => {
+export const unsubscribeChat = () => {
   socket.off(CHAT);
 };
 
-const sendChat = message => {
+export const sendChat = message => {
   socket.emit(CHAT, message);
 };
 
-const changeName = newName => {
+export const changeName = newName => {
   socket.emit(C_NAME, newName);
 };
 
 export const audioURL = ("http://localhost:5000/audio");
 
-const subscribeAudio = cb => {
+export const subscribeAudio = cb => {
   socket.on(AUDIO, data => cb(data));
 };
 
-const unsubscribeAudio = () => {
+export const unsubscribeAudio = () => {
   socket.off(AUDIO);
 };
 
-export { subscribeChat, unsubscribeChat, sendChat, changeName, subscribeAudio, unsubscribeAudio };
+export const requestRoll = (parameters) => {
+    socket.emit(DICE_ROLL, parameters);
+}
+
+export const subscribeRollResult = cb => {
+    socket.on("ROLL_RESULT", data => cb(data));
+}
+
+export const unsubscribeRollResult = cb => {
+    socket.off("ROLL_RESULT");
+}
--- client/src/components/AudioPlayer.jsx
+++ client/src/components/AudioPlayer.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { subscribeAudio, unsubscribeAudio, audioURL } from "../api";
 
 export const AudioPlayer = () => {
+  const audioRef = useRef(null);
+
+  const handlePlayPause = e => {
+    console.log(e.target.paused);
+    return audioRef.current.paused
+      ? audioRef.current.play()
+      : audioRef.current.pause();
+  };
+  const handleVolume = e => {
+    console.log(e.target.value / 100);
+    audioRef.current.volume = e.target.value / 100;
+  };
   return (
     <div className="AudioPlayerWrapper">
       <div className="AudioPlayer">
-        <audio controls>
+        <audio ref={audioRef} controls>
           <source src={audioURL} />
         </audio>
+        <p>Now Playing</p>
+        <span>
+          <button onClick={handlePlayPause}>Play / Pause</button>
+          <input
+            onChange={handleVolume}
+            type="range"
+            min="0"
+            max="100"
+            step="1"
+          />
+        </span>
       </div>
     </div>
   );
