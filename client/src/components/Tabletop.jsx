56785e822a8d44d3d84af7708888a0f109056d5f
Tisciit <tisciit@gmail.com>
2019-12-13 12:30:45 +0100

    changed Tabletop to use p5 js

--- client/src/components/Tabletop.jsx
+++ client/src/components/Tabletop.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
+import P5Wrapper from "react-p5-wrapper";
 import "./Tabletop.css";
 
 export const Tabletop = props => {
-  const refGrid = useRef(null);
+  const sketchContainer = useRef(null);
 
   const MODES = {
     SELECT: 0,
@@ -13,157 +14,69 @@ export const Tabletop = props => {
 
   const [mode, setMode] = useState(MODES.SELECT);
   const [zoom, setZoom] = useState(1);
-  const [xOffset, setXOffset] = useState(0);
-  const [yOffset, setYOffset] = useState(0);
-  const [viewBox, setViewBox] = useState(
-    `${xOffset} ${yOffset} ${zoom * 100} ${zoom * 100}`
-  );
-  const [selection, setSelection] = useState(null);
+  // const [xOffset, setXOffset] = useState(0);
+  // const [yOffset, setYOffset] = useState(0);
 
-  useEffect(() => {
-    setViewBox(`${xOffset} ${yOffset} ${zoom * 100} ${zoom * 100}`);
-  }, [yOffset, xOffset, zoom]);
+  const sketch = p => {
+    let backgroundLayer;
+    let drawingLayer;
+    //Zoom is based on component-state
+    //#region
+    const RADIUS = 50;
 
-  useEffect(() => {
-    const classList = {
-      CURSORPOINTER: "cursorPointer",
-      CURSORMOVE: "cursorMove"
+    p.setup = () => {
+      const COLS = props.COLS || 160;
+      const ROWS = props.ROWS || 90;
+      const width = RADIUS * COLS;
+      const height = RADIUS * ROWS;
+      p.createCanvas(width, height);
+      backgroundLayer = p.createGraphics(width, height);
+
+      const h = RADIUS * Math.sin(Math.PI / 3);
+      const offset = 1.5 * RADIUS;
+
+      let i = 0;
+      for (let y = 0; y < height; y += h) {
+        i++;
+        const o = i % 2 === 0 ? offset : 0;
+        for (let x = o; x < width; x += 3 * RADIUS) {
+          backgroundLayer.fill(backgroundLayer.color(255, 0, 0));
+          backgroundLayer.noStroke();
+          backgroundLayer.strokeWeight(1);
+          backgroundLayer.ellipse(x, y, 2);
+          calculateEdges(x, y);
+        }
+      }
+
+      function calculateEdges(x, y) {
+        backgroundLayer.noFill();
+        backgroundLayer.stroke(255);
+        backgroundLayer.beginShape();
+        for (let a = 0; a < 6; a++) {
+          let x_ = x + RADIUS * Math.cos((a * Math.PI) / 3);
+          let y_ = y + RADIUS * Math.sin((a * Math.PI) / 3);
+          backgroundLayer.vertex(x_, y_);
+        }
+
+        backgroundLayer.endShape(p.CLOSE);
+      }
+
+      drawingLayer = p.createGraphics(width, height);
+      p.image(backgroundLayer, 0, 0);
     };
 
-    //Remove all classes
-    for (let o in Object.getOwnPropertyNames(classList)) {
-      refGrid.current.classList.remove(classList[o]);
-    }
-
-    switch (mode) {
-      case MODES.SELECT:
-        refGrid.current.classList.add("cursorDefault");
-        break;
-      case MODES.DRAG:
-        refGrid.current.classList.add("cursorMove");
-        break;
-
-      default:
-        break;
-    }
-  }, [mode, MODES.DRAG, MODES.SELECT]);
-  //Thanks to https://stackoverflow.com/a/56823542/12512574 for basically providing the SVG Hex Layout creation script!
-  //Create Hex Grid on mount
-  useEffect(() => {
-    const SVG_NS = "http://www.w3.org/2000/svg";
-    const R = 5;
-    const COLS = props.cols || 20;
-    const ROWS = props.rows || 20;
-    const SVGWIDTH = COLS * (R * 1.5);
-    const SVGHEIGHT = ROWS * (R * 1.5);
-
-    const H = SVGHEIGHT,
-      W = SVGWIDTH;
-    const h = R * Math.sin(Math.PI / 3);
-    const offset = 1.5 * R;
-
-    let i = 0;
-    for (let y = 0; y < H; y += h) {
-      i++;
-      const o = i % 2 === 0 ? offset : 0;
-      for (let x = o; x < W; x += 3 * R) {
-        calulcateEdges(x, y);
+    p.draw = () => {
+      p.clear();
+      if (p.mouseIsPressed) {
+        drawingLayer.ellipse(p.mouseX, p.mouseY, 10);
       }
-    }
 
-    function calulcateEdges(x, y) {
-      let points = "";
+      const w = p.width * zoom;
+      const h = p.height * zoom;
 
-      //for a given xy, calculate the points for the 6 edges at 0 - 6 pi / 3
-      for (let a = 0; a < 6; a++) {
-        let obj = {};
-        obj.x = x + R * Math.cos((a * Math.PI) / 3);
-        obj.y = y + R * Math.sin((a * Math.PI) / 3);
-        points += `${obj.x}, ${obj.y} `;
-      }
-      const cell = appendSVGelmt(
-        { points: points },
-        "polygon",
-        refGrid.current
-      );
-      //cell.addEventListener("click", () => {
-      //  console.log(x / R, y / R);
-      //});
-      cell.classList.add("static");
-    }
-
-    function appendSVGelmt(o, tag, parent) {
-      let elmt = document.createElementNS(SVG_NS, tag);
-      for (let name in o) {
-        if (o.hasOwnProperty(name)) {
-          elmt.setAttributeNS(null, name, o[name]);
-        }
-      }
-      parent.appendChild(elmt);
-      return elmt;
-    }
-  }, [props.cols, props.rows]);
-  //
-
-  const SVGMOUSEDOWN = e => {
-    switch (mode) {
-      case MODES.SELECT:
-        setSelection(e.target);
-        break;
-
-      case MODES.DRAG:
-        e.stopPropagation();
-        setSelection(e.target);
-        console.log("I am here");
-        console.log(e.clientX, e.clientY);
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  const SVGMOUSEDRAG = e => {
-    console.log("dragging");
-    switch (mode) {
-      case MODES.SELECT:
-        break;
-
-      case MODES.DRAG:
-        if (selection !== null) {
-          const coords = getMousePosition(e, selection);
-          console.log(coords);
-          setXOffset(coords.x);
-          setYOffset(coords.y);
-        }
-        break;
-
-      default:
-        break;
-    }
-
-    function getMousePosition(evt, svg) {
-      const CTM = svg.getScreenCTM();
-      return {
-        x: (evt.clientX - CTM.e) / CTM.a,
-        y: (evt.clientY - CTM.f) / CTM.d
-      };
-    }
-  };
-
-  const SVGMOUSEUP = e => {
-    switch (mode) {
-      case MODES.SELECT:
-        break;
-
-      case MODES.DRAG:
-        e.stopPropagation();
-        setSelection(null);
-        break;
-
-      default:
-        break;
-    }
+      p.image(backgroundLayer, 0, 0, w, h);
+      p.image(drawingLayer, 0, 0, w, h);
+    };
   };
 
   return (
@@ -177,7 +90,6 @@ export const Tabletop = props => {
         >
           SELECT
         </button>
-
         <button
           className="btn_round"
           onClick={() => {
@@ -212,13 +124,8 @@ export const Tabletop = props => {
         </button>
         {mode}
       </div>
-      <div
-        className="svgContainer"
-        onMouseDown={SVGMOUSEDOWN}
-        onMouseUp={SVGMOUSEUP}
-        onDrag={SVGMOUSEDRAG}
-      >
-        <svg className="svgGrid" ref={refGrid} viewBox={viewBox} />
+      <div className="svgContainer">
+        <P5Wrapper ref={sketchContainer} sketch={sketch} />
       </div>
     </div>
   );
